1e5e302581104828fb32e60fcb7af151b7511b5f
officialamansinha <aman.sinha.connect@gmail.com>
2026-06-21 11:06:23 +0530

    DB Migration team_users Table

--- database/migrations/2026_06_20_202612_create_team_user_table.php
+++ database/migrations/2026_06_20_202612_create_team_user_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('team_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('team_id')->constrained('teams')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->enum('role',['owner','manager','member'])->default('member');
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
